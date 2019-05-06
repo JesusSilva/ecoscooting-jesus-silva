4626ff49f74256941ea68a98b2157a50ace3376f
Jesús Silva Méndez <jesus@192.168.1.127>
2019-05-06 20:12:59 +0200

    feat (countries-list): added view, logic and styles

--- src/app/components/countries-list/countries-list.component.ts
+++ src/app/components/countries-list/countries-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { CountryService } from '../../services/country.service';
+import { Country } from './../../classes/countries';
 
 @Component({
   selector: 'app-countries-list',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountriesListComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['nombre', 'alpha3code', 'capital', 'region', 'bandera'];
+  dataSource: MatTableDataSource<Country>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(
+    private countryService: CountryService,
+    private router: Router,
+  ) {}
 
   ngOnInit() {
+    this.retriveCountriesList();
   }
 
+  retriveCountriesList() {
+    this.countryService.getCountriesList()
+    .subscribe(
+      response => {
+        this.dataSource = new MatTableDataSource(response);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error => console.log(error)
+    );
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  navigateToDetails(alpha3Code) {
+    this.router.navigate(['/detail/' + alpha3Code]);
+  }
 }
